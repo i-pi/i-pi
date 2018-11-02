f5b4be783246e93a1cc001e6d0328f3b37dc9216
Ondrej Marsalek <ondrej.marsalek@gmail.com>
2018-11-02 14:19:47 +0100

    Get rid of dependence on nose

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -8,39 +8,40 @@ import os
 import subprocess
 import shlex
 
-from nose import with_setup
-
-FNULL = open(os.devnull, 'w')
+import pytest
 
 
 def run_command(cmd):
     """Runs `cmd` in doc directory."""
     cwd = os.getcwd()
+    f_null = open(os.devnull, 'w')
     os.chdir(os.path.join(os.path.split(__file__)[0], "..", "doc"))
-    ret = subprocess.call(shlex.split(cmd), stdout=FNULL, stderr=FNULL)
+    ret = subprocess.call(shlex.split(cmd), stdout=f_null, stderr=f_null)
+    f_null.close()
     os.chdir(cwd)
     return ret
 
 
+@pytest.fixture
 def distclean():
     """Prepare for documentation build testing."""
     run_command("make distclean")
 
 
+@pytest.fixture
 def clean():
     """Clean up the documentation build after testing."""
+    yield
     run_command("make clean")
 
 
-@with_setup(distclean, None)
-def test_make_aux():
+def test_make_aux(distclean):
     """doc: run make aux"""
     ret = run_command("make aux")
     assert ret == 0
 
 
-@with_setup(None, clean)
-def test_make():
+def test_make(clean):
     """doc: run make"""
     ret = run_command("make")
     assert ret == 0
