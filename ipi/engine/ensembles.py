c8a94cbc07efbb05f45171e2bf1b83f698fd3bd5
Ondrej Marsalek <ondrej.marsalek@gmail.com>
2018-12-12 16:40:33 +0100

    Update ipi/engine/ensembles.py
    Co-Authored-By: ceriottm <ceriottm@users.noreply.github.com>

--- ipi/engine/ensembles.py
+++ ipi/engine/ensembles.py
@@ -201,7 +201,7 @@ class Ensemble(dobject):
         ensembles.
         """
 
-        eham = self.nm.vspring + self.nm.kin +self.forces.pot
+        eham = self.nm.vspring + self.nm.kin + self.forces.pot
 
         eham += self.bias.pot   # bias
 
