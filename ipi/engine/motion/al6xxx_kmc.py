c4380e90dfdf80d7476efd96f5b81dc3d2ef82bb
Michele Ceriotti <ceriottm@users.noreply.github.com>
2019-03-12 09:23:50 +0100

    Update al6xxx_kmc.py

--- ipi/engine/motion/al6xxx_kmc.py
+++ ipi/engine/motion/al6xxx_kmc.py
@@ -144,6 +144,8 @@ class AlKMC(Motion):
         self.fixatoms = np.asarray([])
         self.fixcom = True
         self.geop = [None] * self.neval
+        # geop should not trigger exit if there is early convergence, but just carry on.
+        # we hard-code this option to avoid early-termination that would be hard to debug for a user
         geop["exit_on_convergence"] = False
         for i in xrange(self.neval):
             # geometry optimizer should not have *any* hystory dependence
