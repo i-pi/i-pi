1582e1868483f76eb2140fd1ad5c866b052e7a03
George Trenins <gt317@cam.ac.uk>
2019-04-29 19:23:11 +0100

    Implement holonomic constraints for quasi-centroid constrained propagation of water (Eckart as yet untested)

--- ipi/engine/__init__.py
+++ ipi/engine/__init__.py
@@ -9,4 +9,4 @@ Modules used to run simulations.
 
 __all__ = ["atoms", "cell", "simulation", "forces", "ensembles", "properties",
            "thermostats", "barostats", "beads", "outputs", "normalmodes",
-           "initializer", "system", "paratemp"]
+           "initializer", "system", "paratemp", "constraints"]
