d5743252ee64a3e1335a702c3bf57c71c57c7c28
George Trenins <gt317@cam.ac.uk>
2019-04-29 19:25:57 +0100

    Revert "Implement holonomic constraints for quasi-centroid constrained"
    This reverts commit 1582e1868483f76eb2140fd1ad5c866b052e7a03.

--- ipi/engine/__init__.py
+++ ipi/engine/__init__.py
@@ -9,4 +9,4 @@ Modules used to run simulations.
 
 __all__ = ["atoms", "cell", "simulation", "forces", "ensembles", "properties",
            "thermostats", "barostats", "beads", "outputs", "normalmodes",
-           "initializer", "system", "paratemp", "constraints"]
+           "initializer", "system", "paratemp"]
