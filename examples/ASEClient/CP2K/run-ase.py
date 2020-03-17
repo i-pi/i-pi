a93e4b4f2d371357ab82ff19c5b77b305c1dafb3
Yair Litman <yairlitman@gmail.com>
2020-03-17 15:13:16 +0100

    Add CP2K example with ASEClient

---
+++ examples/ASEClient/CP2K/run-ase.py
@@ -0,0 +1,34 @@
+import os
+
+from ase.calculators.socketio import SocketClient
+from ase.io import read
+from ase.calculators.cp2k import CP2K
+from ase.test import NotAvailable
+
+####### Define atoms object  ################
+
+atoms = read("init.xyz", 0)
+
+####### Set CP2K calculator #################
+
+workdir = "CP2Ktest"
+aux_settings = {"label": workdir}
+
+if "ASE_CP2K_COMMAND" not in os.environ:
+   print('$ASE_CP2K_COMMAND not defined')
+   print('You can try with:\n "export ASE_CP2K_COMMAND=/usr/bin/cp2k_shell" \n\n')
+   raise NotAvailable('$ASE_CP2K_COMMAND not defined')
+
+calc = CP2K(**aux_settings)
+
+atoms.set_calculator(calc)
+
+################## Create Client ############################
+#inet
+port=10200
+host='localhost'
+client = SocketClient(host=host, port=port)
+
+client.run(atoms)
+
+
