5d4592ee731e56661b00b3ced3777a4de9683e86
Venkat Kapil <venkat.kapil@gmail.com>
2020-03-25 11:26:06 +0100

    1. Removed ffquip and the associated examples.

--- ipi/inputs/simulation.py
+++ ipi/inputs/simulation.py
@@ -97,7 +97,6 @@ class InputSimulation(Input):
               "system_template": (InputSysTemplate, {"help": InputSysTemplate.default_help}),
               "ffsocket": (iforcefields.InputFFSocket, {"help": iforcefields.InputFFSocket.default_help}),
               "fflj": (iforcefields.InputFFLennardJones, {"help": iforcefields.InputFFLennardJones.default_help}),
-              "ffquip": (iforcefields.InputFFQUIP, {"help": iforcefields.InputFFQUIP.default_help}),
               "ffdebye": (iforcefields.InputFFDebye, {"help": iforcefields.InputFFDebye.default_help}),
               "ffplumed": (iforcefields.InputFFPlumed, {"help": iforcefields.InputFFPlumed.default_help}),
               "ffyaff": (iforcefields.InputFFYaff, {"help": iforcefields.InputFFYaff.default_help}),
@@ -155,10 +154,6 @@ class InputSimulation(Input):
                     _iobj = iforcefields.InputFFLennardJones()
                     _iobj.store(_obj)
                     self.extra[_ii] = ("fflj", _iobj)
-                elif isinstance(_obj, eforcefields.FFQUIP):
-                    _iobj = iforcefields.InputFFQUIP()
-                    _iobj.store(_obj)
-                    self.extra[_ii] = ("ffquip", _iobj)
                 elif isinstance(_obj, eforcefields.FFDebye):
                     _iobj = iforcefields.InputFFDebye()
                     _iobj.store(_obj)
@@ -208,8 +203,8 @@ class InputSimulation(Input):
                 syslist.append(v.fetch())
             elif k == "system_template":
                 syslist += v.fetch()  # this will actually generate automatically a bunch of system objects with the desired properties set automatically to many values
-            elif k == "ffsocket" or k == "fflj" or k == 'ffquip' or k == "ffdebye" or k == "ffplumed" or k == "ffsgdml" or k== "ffyaff":
-                info(" # Fetching" + k, verbosity.low)
+            elif k == "ffsocket" or k == "fflj" or k == "ffdebye" or k == "ffplumed" or k == "ffsgdml" or k== "ffyaff":
+                info(" # @simulation: Fetching" + k, verbosity.low)
                 fflist.append(v.fetch())
 
         # this creates a simulation object which gathers all the little bits
