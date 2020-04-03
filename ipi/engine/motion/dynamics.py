44788bc127134ab8dd368863f44e9701b2ca4b82
Mariana Rossi <rossi@theomac15.fritz.box>
2020-04-03 16:16:25 +0200

    Just cosmetic change -- more logical. In the previous commit,   I meant "center of mass" and not centroid.

--- ipi/engine/motion/dynamics.py
+++ ipi/engine/motion/dynamics.py
@@ -68,9 +68,9 @@ class Dynamics(Motion):
         if thermostat is None:
             self.thermostat = Thermostat()
         else:
-            self.thermostat = thermostat
             if (thermostat.__class__.__name__ is ("ThermoPILE_G" or "ThermoNMGLEG ")) and (len(fixatoms)>0):
-                softexit.trigger("!! Sorry, fixed atoms, nbeads > 1 and global thermostat on the centroid not supported. Use a local thermostat. !!")
+                softexit.trigger("!! Sorry, fixed atoms and global thermostat on the centroid not supported. Use a local thermostat. !!")
+            self.thermostat = thermostat
 
         if nmts is None or len(nmts) == 0:
             dd(self).nmts = depend_array(name="nmts", value=np.asarray([1], int))
