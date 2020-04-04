c916495b6c201c320aa009fbf3a9f3335d2dd004
Mariana Rossi <rossi@theomac15.fritz.box>
2020-04-04 17:11:34 +0200

    Disabling for now the ability to use global thermostats on the centroid and fixed atoms at the same time.   Simple fixes are not general enough to cover all cases (non-trivial constraints on the system, fixed center of mass, etc.)   Need separate work to handle fixdof separately. Make it an array, add it to the constraint class. etc.

--- ipi/inputs/motion/motion.py
+++ ipi/inputs/motion/motion.py
@@ -24,6 +24,7 @@ Classes:
 import numpy as np
 from copy import copy
 import ipi.engine.initializer
+from ipi.utils.softexit import softexit
 
 from ipi.engine.motion import Motion, Dynamics, ConstrainedDynamics, Replay, GeopMotion, NEBMover,\
     DynMatrixMover, MultiMotion, AlchemyMC, InstantonMotion,\
@@ -171,7 +172,7 @@ class InputMotionBase(Input):
             raise ValueError("Cannot store Mover calculator of type " + str(type(sc)))
 
         if (sc.fixcom is True) and (len(sc.fixatoms) > 0):
-            raise ValueError("Fixed atoms break translational invariance, and so should be used with <fixcom> False </fixcom>. You can disable this error if you know what you are doing.")
+            softexit.trigger("Fixed atoms break translational invariance, and so should be used with <fixcom> False </fixcom>. You can disable this error if you know what you are doing.")
 
         if tsc == 0:
             self.file.store(sc.intraj)
