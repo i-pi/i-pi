c590283de5998bf9510b4935224bee8e877bb6b8
Yair Litman <yairlitman@gmail.com>
2020-05-27 20:25:13 +0200

    black formatting

--- ipi_tests/regression/runstools.py
+++ ipi_tests/regression/runstools.py
@@ -15,25 +15,29 @@ def get_info_test(parent):
 
     for ff in folders:
         if os.path.isfile(Path(ff) / "input.xml"):
-             driver_info = list()
-             try:
+            driver_info = list()
+            try:
                 with open(Path(ff) / "driver.txt") as f:
-                    ncount =0
+                    ncount = 0
                     while True:
-                          line = f.readline()
-                          if not line:
-                              break
-                          else:
-                              driver, address, port , mode = line.split()             
-                              driver_info.append((driver, address, port, mode))
-                              ncount +=1
+                        line = f.readline()
+                        if not line:
+                            break
+                        else:
+                            driver, address, port, mode = line.split()
+                            driver_info.append((driver, address, port, mode))
+                            ncount += 1
                     reg_tests.append((ff, driver_info))
                     if ncount == 0:
-                         raise ValueError('driver.txt is empty')
-             except FileNotFoundError:
-                  raise FileNotFoundError('A driver.txt file is needed for each reg_text to be executed')
-             except ValueError:
-                  raise ValueError('Please specify "model address      port mode" inside driver.txt')
+                        raise ValueError("driver.txt is empty")
+            except FileNotFoundError:
+                raise FileNotFoundError(
+                    "A driver.txt file is needed for each reg_text to be executed"
+                )
+            except ValueError:
+                raise ValueError(
+                    'Please specify "model address      port mode" inside driver.txt'
+                )
 
     return reg_tests
 
@@ -55,8 +59,8 @@ class Runner(object):
             time.sleep(2)
             driver = list()
             for cmd in cmd2:
-               driver.append(sp.Popen(cmd, shell=True, stdout=sp.PIPE, stderr=sp.PIPE))
-               
+                driver.append(sp.Popen(cmd, shell=True, stdout=sp.PIPE, stderr=sp.PIPE))
+
             self._check_error(ipi)
             self._check_properties(cwd)
 
@@ -82,15 +86,18 @@ class Runner(object):
         print(ipi_error)
         assert "" == ipi_error
 
-    def _check_properties(self,cwd):
+    def _check_properties(self, cwd):
         try:
-           ref_output = np.loadtxt(Path(cwd)/'ref_simulation.out')
+            ref_output = np.loadtxt(Path(cwd) / "ref_simulation.out")
         except IOError:
-           raise ('Please provide a refence properties output named "ref_simulation.out"')
+            raise (
+                'Please provide a refence properties output named "ref_simulation.out"'
+            )
         except ValueError:
-            raise ValueError("Please check ref_simulation.out in {}".format(str(self.parent)))
-   
-        test_output = np.loadtxt(self.tmp_dir/'simulation.out')
- 
-        np.testing.assert_array_equal( test_output, ref_output)
+            raise ValueError(
+                "Please check ref_simulation.out in {}".format(str(self.parent))
+            )
 
+        test_output = np.loadtxt(self.tmp_dir / "simulation.out")
+
+        np.testing.assert_array_equal(test_output, ref_output)
--- ipi_tests/regression/test_run.py
+++ ipi_tests/regression/test_run.py
@@ -6,11 +6,12 @@ import os
 import shutil
 import tempfile
 from ipi_tests.reg_test.runstools import Runner, get_info_test
+
 """ Run regression test """
 
 main_folder = Path(__file__).parent
 call_ipi = "i-pi input.xml"
-call_driver = "i-pi-driver" 
+call_driver = "i-pi-driver"
 
 reg_tests = get_info_test(main_folder)
 print("We have found {} reg_tests".format(len(reg_tests)))
@@ -22,17 +23,17 @@ def test_cmd(test_info):
 
     cmd2 = list()
     for t in test_info[1]:
-       if t[3] == 'unix':
-          cmd2.append(call_driver + " -m {} -h {} -u ".format(t[0],t[1]))
-       elif t[3] == 'inet':
-          cmd2.append(call_driver + " -m {} -h {} -p {}".format(t[0],t[1],t[2]))
-       else:
-          raise ValueError('Driver mode has to be either unix or inet')
+        if t[3] == "unix":
+            cmd2.append(call_driver + " -m {} -h {} -u ".format(t[0], t[1]))
+        elif t[3] == "inet":
+            cmd2.append(call_driver + " -m {} -h {} -p {}".format(t[0], t[1], t[2]))
+        else:
+            raise ValueError("Driver mode has to be either unix or inet")
     runner._run(call_ipi, cmd2, test_info[0])
 
 
 if __name__ == "__main__":
-    
+
     for test_info in reg_tests:
         print("Running {} ".format(test_info[0]))
         test_cmd(test_info)
