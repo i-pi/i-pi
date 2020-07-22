6a9102de4273132f6a7cbc74cea0f1388412ccd2
Yair Litman <yairlitman@gmail.com>
2020-07-22 12:23:10 +0200

    Add more docstrings and update README

--- ipi_tests/regression_tests/runstools.py
+++ ipi_tests/regression_tests/runstools.py
@@ -12,6 +12,10 @@ import tempfile
 
 
 def get_info_test(parent):
+    """ This function recursively searches for examples
+    and check for the presence of the required additional info
+    required (i.e. driver.txt)
+    """
     folders = [x[0] for x in os.walk(parent)]
     reg_tests = list()
 
@@ -45,12 +49,26 @@ def get_info_test(parent):
 
 
 class Runner(object):
+    """ This class handles the modification the creation of tmp directories,
+    the i-pi call, the driver call, and finally
+    it checks that the generated outputs are the expected ones.
+    """
+
     def __init__(self, parent, check_errors=True, check_prop=True):
+        """ Store parent directory and commands to call i-pi and driver """
         self.parent = parent
         self.check_error = check_errors
         self.check_prop = check_prop
 
     def _run(self, cmd1, cmd2, cwd):
+        """ This function tries to run the example in a tmp folder and
+        checks if ipi has ended without error.
+        After that the output is checked against a reference
+        arguments:
+            cwd: folder where all the original examples files are stored
+            cmd1: command to call i-pi
+            cmd2: list of commands to call drivers
+        """
 
         try:
             for filename in glob.glob("/tmp/ipi_*"):
@@ -92,12 +110,15 @@ class Runner(object):
             raise ValueError("{}".format(str(cwd)))
 
     def _check_error(self, ipi):
+        """ This function checks if ipi has exited with errors"""
         ipi_error = ipi.communicate(timeout=30)[1].decode("ascii")
         print(ipi_error)
         assert "" == ipi_error
 
     def _check_properties(self, cwd):
+        """ This function checks if the simulation.out is 'all_close' to the reference file provided"""
 
+        # TODO: Extend this to other types of checks
         try:
             ref_output = np.loadtxt(Path(cwd) / "ref_simulation.out")
         except IOError:
