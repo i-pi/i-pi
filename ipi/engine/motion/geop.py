7cbba13af31e35fcfc00b3a8e30c4beb6dd5929f
litman90 <yairlitman@gmail.com>
2020-10-01 15:12:46 +0200

    Revert "Regtest infrastructure"

--- ipi/engine/motion/geop.py
+++ ipi/engine/motion/geop.py
@@ -432,7 +432,6 @@ class BFGSOptimizer(DummyOptimizer):
             self.d += dstrip(self.forces.f) / np.sqrt(
                 np.dot(self.forces.f.flatten(), self.forces.f.flatten())
             )
-
             if len(self.fixatoms) > 0:
                 for dqb in self.d:
                     dqb[self.fixatoms * 3] = 0.0
@@ -648,6 +647,7 @@ class LBFGSOptimizer(DummyOptimizer):
 
         if step == 0:
             info(" @GEOP: Initializing L-BFGS", verbosity.debug)
+            print(self.d)
             self.d += dstrip(self.forces.f) / np.sqrt(
                 np.dot(self.forces.f.flatten(), self.forces.f.flatten())
             )
