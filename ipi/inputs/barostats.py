05c0c77c1cca94b3eddc1a3e12ab2f3aeb78e465
Venkat Kapil <venkat.kapil@gmail.com>
2021-01-06 21:16:09 +0000

    bugfix in default value of hmask

--- ipi/inputs/barostats.py
+++ ipi/inputs/barostats.py
@@ -94,7 +94,7 @@ class InputBaro(Input):
         "hmask": (
             InputArray,
             {
-                "default": input_default(factory=np.zeros, args=(0,)),
+                "default": np.ones((3,3)),
                 "dtype": float,
                 "help": "a matrix of 0s and 1s used to specify which components of the cell are allowed to fluctuate.",
             },
