7417c296de41480d17f698111852292b2b70cd44
Michele Ceriotti <michele.ceriotti@gmail.com>
2021-01-07 15:42:36 +0100

    Change default of i-pi-style so it runs with the current folder as root

--- tools/py/style.py
+++ tools/py/style.py
@@ -14,7 +14,7 @@ import sys
 
 formatter = "black"
 linter = "flake8"
-cwd_repo = Path(__file__).resolve().parents[2]
+cwd_repo = Path('./').resolve()
 
 
 def main(check_mode, folder_path, file_path):
