4e542eb8e758d6ad9e0a71f1d42fa02f55b97fb7
Yair Litman <yairlitman@gmail.com>
2021-01-12 19:12:17 +0100

    check if the lint workflow fails

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 
 
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
 setup(
