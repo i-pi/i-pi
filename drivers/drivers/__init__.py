d6e9507e4050fc21eff85abf2fe61f091e462145
Yair Litman <yairlitman@gmail.com>
2021-02-23 16:20:08 +0100

    add spline driver

--- drivers/drivers/__init__.py
+++ drivers/drivers/__init__.py
@@ -1,9 +1,14 @@
 """ Small functions/classes providing access to driver PES to be called from driver.py """
 
 from .harmonic import Harmonic_driver
+from .spline import Spline_driver
 from .dummy import Dummy_driver
 
-__all__ = ["__drivers__", "Dummy_driver", "Harmonic_driver"]
+__all__ = ["__drivers__", "Dummy_driver", "Harmonic_driver", "Spline_driver"]
 
 # dictionary linking strings
-__drivers__ = {"dummy": Dummy_driver, "harmonic": Harmonic_driver}
+__drivers__ = {
+    "dummy": Dummy_driver,
+    "harmonic": Harmonic_driver,
+    "spline": Spline_driver,
+}
