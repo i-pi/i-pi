1f16f69b8a9e2ca2dae8cb4c029c85d094d87a4e
Michele Ceriotti <michele.ceriotti@gmail.com>
2021-04-21 00:16:48 +0200

    Fixed missing librascal module (still haven't merged the librascal branch!)

--- drivers/py/pes/__init__.py
+++ drivers/py/pes/__init__.py
@@ -2,13 +2,13 @@
 
 from .dummy import Dummy_driver
 from .harmonic import Harmonic_driver
-from .rascal import Rascal_driver
+#from .rascal import Rascal_driver
 
-__all__ = ["__drivers__", "Dummy_driver", "Harmonic_driver", "Rascal_driver"]
+__all__ = ["__drivers__", "Dummy_driver", "Harmonic_driver"]#, "Rascal_driver"]
 
 # dictionary linking strings
 __drivers__ = {
     "dummy": Dummy_driver,
     "harmonic": Harmonic_driver,
-    "rascal": Rascal_driver,
+#    "rascal": Rascal_driver,
 }
