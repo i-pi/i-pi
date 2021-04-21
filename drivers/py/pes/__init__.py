d4b4cd87ef309cae62b477962621ae058aad18dc
Michele Ceriotti <ceriottm@users.noreply.github.com>
2021-04-21 14:28:49 +0200

    Interface to use librascal potentials through driver.py (#171)
    * Added a python-side librascal forcefield
    * Implemented the rascal driver i-pi side
    * using NST example so we check also virial is fine.
    * Change the driver.f90 CMDline parsing to use -a for hostname
         I'm leaving -h for back-compatibility, but indeed it's best to stick with -h == help, as argparse does
    * Update rascal driver for the new "generic" rascal calculator
       This basically involves making some unit conversions that we don't want
       librascal to have to worry about
    * Removed the FFRascal class, as Librascal is supposed to be run with the socket interface and the rascal driver
        ipi/engine/forcefields.py
    * Leave RascalCalc throw a meaningful error rather than catching all exceptions
    * Copied over examples from librascal/
      Added example readme
    
    Co-authored-by: Max Veit <maxdveit@gmail.com>
    Co-authored-by: Lorenzo Gigli <gigli@epfl.ch>

--- drivers/py/pes/__init__.py
+++ drivers/py/pes/__init__.py
@@ -1,9 +1,14 @@
 """ Small functions/classes providing access to driver PES to be called from driver.py """
 
-from .harmonic import Harmonic_driver
 from .dummy import Dummy_driver
+from .harmonic import Harmonic_driver
+from .rascal import Rascal_driver
 
-__all__ = ["__drivers__", "Dummy_driver", "Harmonic_driver"]
+__all__ = ["__drivers__", "Dummy_driver", "Harmonic_driver", "Rascal_driver"]
 
 # dictionary linking strings
-__drivers__ = {"dummy": Dummy_driver, "harmonic": Harmonic_driver}
+__drivers__ = {
+    "dummy": Dummy_driver,
+    "harmonic": Harmonic_driver,
+    "rascal": Rascal_driver,
+}
