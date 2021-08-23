f8fb00dfaa8bcc81cbacb670af32e5b844e29179
Michele Ceriotti <michele.ceriotti@epfl.ch>
2021-08-23 11:34:05 +0200

    Fixed cell transpose error in i-pi/librascal interface

--- drivers/py/pes/rascal.py
+++ drivers/py/pes/rascal.py
@@ -45,13 +45,14 @@ class Rascal_driver(Dummy_driver):
     def __call__(self, cell, pos):
         """Get energies, forces, and stresses from the librascal model"""
         pos_rascal = unit_to_user("length", "angstrom", pos)
-        cell_rascal = unit_to_user("length", "angstrom", cell)
+        # librascal expects ASE-format, cell-vectors-as-rows
+        cell_rascal = unit_to_user("length", "angstrom", cell.T)
         # Do the actual calculation
         pot, force, stress = self.rascal_calc.calculate(pos_rascal, cell_rascal)
         pot_ipi = unit_to_internal("energy", "electronvolt", pot)
         force_ipi = unit_to_internal("force", "ev/ang", force)
         # The rascal stress is normalized by the cell volume (in rascal units)
-        vir_rascal = -1 * stress * det_ut3x3(cell_rascal)
-        vir_ipi = unit_to_internal("energy", "electronvolt", vir_rascal)
+        vir_rascal = -1 * stress * det_ut3x3(cell_rascal)        
+        vir_ipi = unit_to_internal("energy", "electronvolt", vir_rascal.T)
         extras = ""
         return pot_ipi, force_ipi, vir_ipi, extras
