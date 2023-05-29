02e79a60a9dedcaf2cb5045d73d28e3978211764
Mariana Rossi 2 <mariana.rossi@gmail.com>
2023-05-29 10:48:07 +0200

    Diatomic Morse (for exercises)

--- drivers/f90/pes/morse.f
+++ drivers/f90/pes/morse.f
@@ -7,4 +7,16 @@
          dr=nq-r0
          pot=D*(dexp(-2.*a*dr)-2.*dexp(-a*dr))
          force=-2*a*D*(dexp(-a*dr)-dexp(-2.0*a*dr))*q/nq
-      END SUBROUTINE 
+      END SUBROUTINE
+
+      SUBROUTINE getmorsedia(r0,D,a,q,pot,force)
+         IMPLICIT NONE
+         DOUBLE PRECISION r0, D, a, q(2,3), pot, force(2,3)
+         DOUBLE PRECISION nq, dr, diff(3)
+         diff(:)=(q(1,:)-q(2,:))
+         nq=dsqrt(dot_product(diff,diff))
+         dr=nq-r0
+         pot=D*(dexp(-2.*a*dr)-2.*dexp(-a*dr))
+         force(1,:)=2*a*D*(dexp(-a*dr)-dexp(-2.0*a*dr))*diff/nq
+         force(2,:)=2*a*D*(dexp(-a*dr)-dexp(-2.0*a*dr))*diff/nq
+      END SUBROUTINE
