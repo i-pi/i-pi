01f9e35943e3fd80e7a49c52776031303e26317d
Michele Ceriotti <ceriottm@users.noreply.github.com>
2023-11-14 19:02:53 -0800

    Third round of examples clean-up (#278)
    incomplete work towards these goals 
    *  use lowercase names rather than Camel_Case
    *  use underscores rather than hyphen consistently
     * flatten a bit the folder hierarchy - all the feature_type/feature/lammps_something should be just feature_type/feature
    *  use driver whenever possible
    
    ---------
    
    Co-authored-by: litman90 <yairlitman@gmail.com>

---
+++ examples/features/path_integral_molecular_dynamics/cayley_pimd/run.sh
@@ -0,0 +1,16 @@
+ipi=i-pi
+driver="i-pi-driver -m qtip4pf -u -a h2o-cayley"
+sleep_time=4
+
+${ipi} input.xml > log.i-pi & 
+echo "# i-PI is running"
+
+echo "# Waiting for ${sleep_time} (s) before executing driver"
+sleep ${sleep_time}
+
+${driver} > /dev/null & 
+echo "# Driver is running"
+
+wait
+
+echo "# Simulation complete"
