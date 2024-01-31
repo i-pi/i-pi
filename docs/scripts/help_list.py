f7d4d8f736e1fc51e5c54e729d1d5457789c2350
Mariana Rossi 2 <mariana.rossi@gmail.com>
2024-01-31 23:27:37 +0100

    And black

--- docs/scripts/help_list.py
+++ docs/scripts/help_list.py
@@ -88,5 +88,6 @@ def help_list(
         rst_output = open(prefix + ".rst", "w")
         rst_output.write(help_rst(idict, standalone=standalone))
 
+
 if __name__ == "__main__":
     help_list(options.opt, options.prefix, not options.ref)
