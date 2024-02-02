78f2462bee671aed0260e053ca8eb4f185045a9e
Michele Ceriotti <ceriottm@users.noreply.github.com>
2024-02-02 12:55:18 +0100

    Update metatensor.py

--- drivers/py/pes/metatensor.py
+++ drivers/py/pes/metatensor.py
@@ -1,4 +1,5 @@
-"""Interface with metatensor to run machine learning potentials"""
+"""Interface with the [metatensor](https://lab-cosmo.github.io/metatensor/latest/atomistic/index.html)
+calculator, that can be used to perform calculations based on different types of machine learning potentials"""
 
 import sys
 from .ase import ASEDriver
