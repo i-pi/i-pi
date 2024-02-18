f14a29f92b6dcda4cb61a27d8970818201d955b0
Michele Ceriotti <michele.ceriotti@gmail.com>
2024-02-18 16:58:34 +0100

    No reason to install the tests!

--- setup.py
+++ setup.py
@@ -8,13 +8,9 @@ setup(
         *find_packages(exclude=["ipi_tests*"]),
         "ipi._driver",
         "ipi._driver.pes",
-        "ipi.tests",
-        "ipi.tests.regression_tests",
     ],
     package_dir={
         "ipi._driver": "drivers/py",
-        "ipi.tests": "ipi_tests",
     },
-    package_data={"ipi.tests.regression_tests": ["tests/NVE/NVE_1/harmonic_python/*"]},
     scripts=[str(p) for p in Path("bin").iterdir()],
 )
