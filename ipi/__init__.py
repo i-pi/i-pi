570a5c24768805355174f473ec333c7df477b49f
Michele Ceriotti <michele.ceriotti@gmail.com>
2024-02-19 21:07:02 +0100

    Added utilities to parse ipi output files, and also mention this in the docs

--- ipi/__init__.py
+++ ipi/__init__.py
@@ -2,6 +2,18 @@
 The i-PI package.
 """
 
-__all__ = ["clients", "engine", "inputs", "interfaces", "utils", "ipi_global_settings"]
+# expose some utility functions in a more direct way
+from ipi.utils.parsing import read_output, read_trajectory
+
+__all__ = [
+    "clients",
+    "engine",
+    "inputs",
+    "interfaces",
+    "utils",
+    "ipi_global_settings",
+    "read_output",
+    "read_trajectory",
+]
 
 ipi_global_settings = {"floatformat": "%16.8e"}
