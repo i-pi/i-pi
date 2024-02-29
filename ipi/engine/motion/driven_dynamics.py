a87a60f7ade28176858a1c309feb4208594621c3
EliaStocco <eliastocco@gmail.com>
2024-02-29 15:25:00 +0100

    cleaned code and fixed bug due to polymorphism

--- ipi/engine/motion/driven_dynamics.py
+++ ipi/engine/motion/driven_dynamics.py
@@ -15,7 +15,7 @@ import numpy as np
 from ipi.utils.depend import *
 from ipi.engine.motion.eda import EDA
 from ipi.utils.units import Constants
-from ipi.engine.motion.dynamics import NVEIntegrator, Dynamics
+from ipi.engine.motion.dynamics import NVEIntegrator, DummyIntegrator, Dynamics
 
 
 class DrivenDynamics(Dynamics):
@@ -102,163 +102,6 @@ class DrivenDynamics(Dynamics):
 dproperties(DrivenDynamics, ["dt", "nmts", "splitting", "ntemp"])
 
 
-class DummyIntegrator:
-    """No-op integrator for (PI)MD"""
-
-    def __init__(self):
-        pass
-
-    def get_qdt(self):
-        return self.dt * 0.5 / self.inmts
-
-    def get_pdt(self):
-        dtl = 1.0 / self.nmts
-        for i in range(1, len(dtl)):
-            dtl[i] *= dtl[i - 1]
-        dtl *= self.dt * 0.5
-        return dtl
-
-    def get_tdt(self):
-        if self.splitting == "obabo":
-            return self.dt * 0.5
-        elif self.splitting == "baoab":
-            return self.dt
-        else:
-            raise ValueError(
-                "Invalid splitting requested. Only OBABO and BAOAB are supported."
-            )
-
-    def bind(self, motion):
-        """Reference all the variables for simpler access."""
-
-        self.beads = motion.beads
-        self.bias = motion.ensemble.bias
-        self.ensemble = motion.ensemble
-        self.forces = motion.forces
-        self.prng = motion.prng
-        self.nm = motion.nm
-        self.thermostat = motion.thermostat
-        self.barostat = motion.barostat
-        self.fixcom = motion.fixcom
-        self.fixatoms = motion.fixatoms
-        self.enstype = motion.enstype
-        if motion.enstype in EDA.integrators:
-            self.eda = motion.eda
-
-        # no need to dpipe these are really just references
-        self._splitting = motion._splitting
-        self._dt = motion._dt
-        self._nmts = motion._nmts
-
-        # total number of iteration in the inner-most MTS loop
-        self._inmts = depend_value(name="inmts", func=lambda: np.prod(self.nmts))
-        self._nmtslevels = depend_value(name="nmtslevels", func=lambda: len(self.nmts))
-        # these are the time steps to be used for the different parts of the integrator
-        self._qdt = depend_value(
-            name="qdt",
-            func=self.get_qdt,
-            dependencies=[self._splitting, self._dt, self._inmts],
-        )  # positions
-        self._qdt_on_m = depend_array(
-            name="qdt_on_m",
-            value=np.zeros(3 * self.beads.natoms),
-            func=lambda: self.qdt / dstrip(self.beads.m3)[0],
-        )
-        self._pdt = depend_array(
-            name="pdt",
-            func=self.get_pdt,
-            value=np.zeros(len(self.nmts)),
-            dependencies=[self._splitting, self._dt, self._nmts],
-        )  # momenta
-        self._tdt = depend_value(
-            name="tdt",
-            func=self.get_tdt,
-            dependencies=[self._splitting, self._dt, self._nmts],
-        )  # thermostat
-
-        dpipe(self._qdt, self.nm._dt)
-        dpipe(self._dt, self.barostat._dt)
-        dpipe(self._qdt, self.barostat._qdt)
-        dpipe(self._pdt, self.barostat._pdt)
-        dpipe(self._tdt, self.barostat._tdt)
-        dpipe(self._tdt, self.thermostat._dt)
-
-        if motion.enstype == "sc" or motion.enstype == "scnpt":
-            # coefficients to get the (baseline) trotter to sc conversion
-            self.coeffsc = np.ones((self.beads.nbeads, 3 * self.beads.natoms), float)
-            self.coeffsc[::2] /= -3.0
-            self.coeffsc[1::2] /= 3.0
-
-        # check stress tensor
-        self._stresscheck = True
-
-    def pstep(self):
-        """Dummy momenta propagator which does nothing."""
-        pass
-
-    def qcstep(self):
-        """Dummy centroid position propagator which does nothing."""
-        pass
-
-    def step(self, step=None):
-        """Dummy simulation time step which does nothing."""
-        pass
-
-    def pconstraints(self):
-        """This removes the centre of mass contribution to the kinetic energy.
-
-        Calculates the centre of mass momenta, then removes the mass weighted
-        contribution from each atom. If the ensemble defines a thermostat, then
-        the contribution to the conserved quantity due to this subtraction is
-        added to the thermostat heat energy, as it is assumed that the centre of
-        mass motion is due to the thermostat.
-
-        If there is a choice of thermostats, the thermostat
-        connected to the centroid is chosen.
-        """
-
-        if self.fixcom:
-            na3 = self.beads.natoms * 3
-            nb = self.beads.nbeads
-            p = dstrip(self.beads.p)
-            m = dstrip(self.beads.m3)[:, 0:na3:3]
-            M = self.beads[0].M
-            Mnb = M * nb
-
-            dens = 0
-            for i in range(3):
-                pcom = p[:, i:na3:3].sum()
-                dens += pcom**2
-                pcom /= Mnb
-                self.beads.p[:, i:na3:3] -= m * pcom
-
-            self.ensemble.eens += dens * 0.5 / Mnb
-
-        if len(self.fixatoms) > 0:
-            for bp in self.beads.p:
-                m = dstrip(self.beads.m)
-                self.ensemble.eens += 0.5 * np.dot(
-                    bp[self.fixatoms * 3], bp[self.fixatoms * 3] / m[self.fixatoms]
-                )
-                self.ensemble.eens += 0.5 * np.dot(
-                    bp[self.fixatoms * 3 + 1],
-                    bp[self.fixatoms * 3 + 1] / m[self.fixatoms],
-                )
-                self.ensemble.eens += 0.5 * np.dot(
-                    bp[self.fixatoms * 3 + 2],
-                    bp[self.fixatoms * 3 + 2] / m[self.fixatoms],
-                )
-                bp[self.fixatoms * 3] = 0.0
-                bp[self.fixatoms * 3 + 1] = 0.0
-                bp[self.fixatoms * 3 + 2] = 0.0
-
-
-dproperties(
-    DummyIntegrator,
-    ["splitting", "nmts", "dt", "inmts", "nmtslevels", "qdt", "pdt", "tdt", "qdt_on_m"],
-)
-
-
 class EDAIntegrator(DummyIntegrator):
     """Integrator object for simulations using the Electric Dipole Approximation (EDA)
     when an external electric field is applied.
@@ -311,7 +154,8 @@ class EDAIntegrator(DummyIntegrator):
             raise ValueError(
                 "EDAIntegrator is not implemented with the Multiple Time Step algorithm (yet)."
             )
-        super().step(0)
+        # This should call 'NVEIntegrator.step' since 'self' should be an instance of 'EDANVEIntegrator' and not of 'EDAIntegrator'
+        super().step(step)
 
 
 dproperties(EDAIntegrator, ["EDAforces", "mts_time", "time"])
@@ -330,9 +174,4 @@ class EDANVEIntegrator(EDAIntegrator, NVEIntegrator):
             self, level
         )  # the driver is called here: add nuclear and electronic forces (DFT)
         EDAIntegrator.pstep(self, level)  # add the driving forces, i.e. q_e Z @ E(t)
-        pass
-
-    def step(self, step):
-        NVEIntegrator.pstep(self, level=0)
-        EDAIntegrator.pstep(self, level=0)
         pass
