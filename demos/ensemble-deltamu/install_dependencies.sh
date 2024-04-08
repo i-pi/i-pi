ecd0e5f9b775f9305519ee23435774b23f1a2f99
Michele Ceriotti <michele.ceriotti@epfl.ch>
2024-04-08 17:35:27 +0200

    Still no joy

--- demos/ensemble-deltamu/install_dependencies.sh
+++ demos/ensemble-deltamu/install_dependencies.sh
@@ -1,51 +1,13 @@
 #!/bin/bash
 
-#check if rust already installed, if yes, exit with text
-
-# Check if Rust compiler (rustc) is installed
-if command -v rustc &> /dev/null; then
-    echo "Rust is already installed. This script will attempt an install with rust 1.74.0 version, better start in a fresh environment"
-fi
-
-# Check if Cargo is installed
-if command -v cargo &> /dev/null; then
-    echo "Cargo is already installed. This script will attempt an install with rust 1.74.0 version, better start in a fresh environment"
-fi
+conda install -c conda-forge rust python=3.10
+conda install --strict-channel-priority -c plumed/label/masterclass-2022 -c conda-forge plumed py-plumed
 
 git clone -b move-rascaline git@github.com:bananenpampe/H2O.git
 
-# Install cargo v<1.75.0
-mkdir -p "$PWD/rust_installation"
-
-#already fixed in newer rascaline versions
-export MACOSX_DEPLOYMENT_TARGET=11
-
-# Set the installation directories
-export RUSTUP_HOME="$PWD/rust_installation/rustup"
-export CARGO_HOME="$PWD/rust_installation/cargo"
-
-
-# Download and install rustup, and thus Rust, into the specified directories
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-
-# Update the current shell session
-source "${CARGO_HOME}/env"
-
-echo "Rust has been installed in $PWD/rust_installation"
-rustc --version
-
-rustup install 1.74.0
-
-# Set Rust 1.7.4 as the default version for compatibility
-rustup default 1.74.0
-
-# Verify the installation
-rustc --version
-
-
 # pip installs
 pip install cmake numpy
-pip install --extra-index-url https://download.pytorch.org/whl/cpu torch==2.0.1
+pip install --extra-index-url https://download.pytorch.org/whl/cpu torch==2.2.0
 
 export RASCALINE_COMMIT=55ad1f14363812639c865801a6d517d8c5f363f6
 export METATENSOR_OPERATIONS_COMMIT=646f1f49c1d9cc18c3fa0d62e2c15e630b8d9d8c
@@ -68,4 +30,5 @@ pip install -r requirements.txt
 # modifies the i_pi drivers
 cp ./H2O/ipi_driver/lightning.py ../../drivers/py/pes/
 
+export PLUMED_KERNEL=$PWD/plumed-masterclass-2022/lib/libplumedKernel.so
 export MLIP_DIR=$PWD/H2O/driver/
