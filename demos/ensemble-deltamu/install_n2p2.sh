65e8b88fb00c631302be863dced693711bd9c368
bananenpampe <bananenpampe@t-online.de>
2024-05-22 05:10:40 +0000

    update install script due to plumed version conflict in lammps forge

--- demos/ensemble-deltamu/install_n2p2.sh
+++ demos/ensemble-deltamu/install_n2p2.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 conda install conda-forge::lammps
+conda uninstall plumed
 conda install --strict-channel-priority -c plumed/label/ipi-demo -c conda-forge plumed py-plumed
 
 . ../../env.sh
