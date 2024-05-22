aafae19bc1f53b025e20ea68eb9017f712a3b7df
bananenpampe <bananenpampe@t-online.de>
2024-05-22 17:50:37 +0000

    update install script for eventual caching and add second lammps run script

--- demos/ensemble-deltamu/install_n2p2.sh
+++ demos/ensemble-deltamu/install_n2p2.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "Cleaning conda cache"
+
+conda clean -all
+conda install python=3.10
 conda install conda-forge::lammps
 conda uninstall plumed
 conda install --strict-channel-priority -c plumed/label/ipi-demo -c conda-forge plumed py-plumed
