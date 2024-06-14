d79da9bf22e1d6743b234e63b8a29f594f3132e4
Matthias Kellner <bananenpampe@t-online.de>
2024-06-14 02:29:48 +0200

    adds condition to install script, updates rascaline install script

--- demos/ensemble-deltamu/install_rascaline.sh
+++ demos/ensemble-deltamu/install_rascaline.sh
@@ -1,6 +1,38 @@
 #!/bin/bash
 
+# Check if the miniconda3 directory already exists
+if [ ! -d "./miniconda3" ]; then
+    echo "Miniconda is not installed. Installing Miniconda..."
+
+    # Create directory for Miniconda
+    mkdir -p ./miniconda3
+
+    # Download Miniconda
+    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ./miniconda3/miniconda.sh
+
+    # Make the script executable
+    chmod u+rx ./miniconda3/miniconda.sh
+
+    # Execute the installer
+    bash ./miniconda3/miniconda.sh -b -u -p ./miniconda3
+
+    # Remove the installer script
+    rm -rf ./miniconda3/miniconda.sh
+
+    echo "Miniconda installed successfully."
+else
+    echo "Miniconda is already installed. Skipping installation."
+fi
+
+./miniconda3/bin/conda create -n rascaline_install -y
+
+source ./miniconda3/bin/activate rascaline_install
+
+export CONDA_BASE="$PWD/miniconda3/bin"
+
 conda install -c conda-forge cxx-compiler gcc rust python=3.10
+
+echo $CONDA_PREFIX
 
 export PATH="$CONDA_PREFIX/bin:$PATH"
 export LD_LIBRARY_PATH="$CONDA_PREFIX/lib:$LD_LIBRARY_PATH"
