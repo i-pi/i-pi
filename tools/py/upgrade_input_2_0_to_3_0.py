bd4837fd80429d1e111618383bc44359b169fee8
yotamfe <yotam.feldman@gmail.com>
2024-06-20 00:19:53 +0300

    scripts should be pretty

--- tools/py/upgrade_input_2_0_to_3_0.py
+++ tools/py/upgrade_input_2_0_to_3_0.py
@@ -2,6 +2,7 @@ import argparse
 from xml.etree import ElementTree as ET
 import xml.dom.minidom
 
+
 def prettify(elem):
     """
     https://stackoverflow.com/questions/17402323/use-xml-etree-elementtree-to-print-nicely-formatted-xml-files
@@ -9,19 +10,27 @@ def prettify(elem):
     """
     rough_string = ET.tostring(elem)
     reparsed = xml.dom.minidom.parseString(rough_string)
-    return '\n'.join([line for line in reparsed.toprettyxml(indent=' '*2).split('\n') if line.strip()])
+    return "\n".join(
+        [
+            line
+            for line in reparsed.toprettyxml(indent=" " * 2).split("\n")
+            if line.strip()
+        ]
+    )
+
 
 def set_all_ffsocket_pbc_if_default(input_path):
     tree = ET.parse(input_path)
 
     # making the default value of pbc in v2.0 explicit in the input (changed in v3.0)
-    ffsocket_elements = tree.getroot().iter('ffsocket')
+    ffsocket_elements = tree.getroot().iter("ffsocket")
     for ffsocket_element in ffsocket_elements:
-        if 'pbc' not in ffsocket_element.attrib:
-            ffsocket_element.set('pbc', 'True')
+        if "pbc" not in ffsocket_element.attrib:
+            ffsocket_element.set("pbc", "True")
 
     with open(input_path, "wt") as f:
         f.write(prettify(tree.getroot()))
+
 
 def main():
     parser = argparse.ArgumentParser(
@@ -41,5 +50,6 @@ def main():
 
     set_all_ffsocket_pbc_if_default(input_path)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
