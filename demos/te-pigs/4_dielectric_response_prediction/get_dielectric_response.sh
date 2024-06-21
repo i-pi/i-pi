d3737e19415f4ed2d3dd201e648988d2308e013f
Venkat Kapil <venkat.kapil@gmail.com>
2024-06-21 11:12:14 +0100

    small cleanups

--- demos/te-pigs/4_dielectric_response_prediction/get_dielectric_response.sh
+++ demos/te-pigs/4_dielectric_response_prediction/get_dielectric_response.sh
@@ -1,12 +1,15 @@
 # Downloads the mu-alpha-models
 wget -nc https://github.com/venkatkapil24/ML-quantum-vibrational-spectroscopy/raw/main/MACE_dipole_pol.model
 
+# MACE currently doesn't support the extxyz format
+ln -s ../3_production_simulations/simulation.pos_0.extxyz dataset.xyz
+
 # Uses the evaluate script to estimate mu and alpha
 mace_eval_mu_alpha \
-  --configs="../3_production_simulations/simulation.xc.xyz" \
+  --configs="dataset.xyz" \
   --model="MACE_dipole_pol.model" \
-  --output="output.extxyz" \
   --device=cuda \
-  --batch_size=10 \
+  --batch_size=50 \
+  --output="output.extxyz" \
+# Use this flag to also export the spatial derivates of the dipole and polarizability in the extxyz format.
 #  --compute_dielectric_derivatives
-
