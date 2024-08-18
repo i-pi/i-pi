3ec38b6e80dcb483a00a8cbf0262e5e3c3d6f7e5
Michele Ceriotti <michele.ceriotti@gmail.com>
2024-08-18 14:45:25 +0200

    Reverted commit of corrupted empty files

---
+++ ipi/__init__.py
@@ -0,0 +1,20 @@
+"""
+The i-PI package.
+"""
+
+# expose some utility functions in a more direct way
+from ipi.utils.parsing import read_output, read_trajectory
+from ipi.utils.setup import install_driver
+
+__all__ = [
+    "clients",
+    "engine",
+    "inputs",
+    "interfaces",
+    "utils",
+    "ipi_global_settings",
+    "install_driver" "read_output",
+    "read_trajectory",
+]
+
+ipi_global_settings = {"floatformat": "%16.8e"}
