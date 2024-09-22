16b8878e10ebbfd8e2eea0ee0a71644b0b0a5fb7
Vahideh Alizadeh <vahideh.90.alizadeh@gmail.com>
2024-09-22 18:26:38 +0200

    format file with black

--- drivers/py/pes/xtb.py
+++ drivers/py/pes/xtb.py
@@ -7,15 +7,16 @@ Example::
 """
 
 import numpy as np
-import json 
+import json
 
-from ipi.utils.messages import warning
 from ipi.utils.units import unit_to_internal, unit_to_user
 
 try:
     import tblite.interface as tb
 except ImportError as e:
-    raise ModuleNotFoundError("Could not find tblite for xtb driver. Please install tblite-python with mamba") from e
+    raise ModuleNotFoundError(
+        "Could not find tblite for xtb driver. Please install tblite-python with mamba"
+    ) from e
 
 __DRIVER_NAME__ = "xtb"
 __DRIVER_CLASS__ = "TBLiteDriver"
@@ -25,15 +26,17 @@ class TBLiteDriver(object):
     """Base class providing the structure of a PES for the python driver."""
 
     def __init__(
-        self, args="", verbose=False,
+        self,
+        args="",
+        verbose=False,
     ):
         """Initialized dummy drivers"""
         config = json.loads(args)
         try:
-            self.method = config["method"] 
+            self.method = config["method"]
             self.numbers = np.asarray(config["numbers"])
         except KeyError as e:
-            raise ValueError (f"Required key {str(e)} not found.")
+            raise ValueError(f"Required key {str(e)} not found.")
         self.charge = config.get("charge")
         self.uhf = config.get("uhf")
         self.periodic = config.get("periodic")
@@ -49,15 +52,15 @@ class TBLiteDriver(object):
 
         pos = unit_to_user("length", "bohr", pos)
         cell = unit_to_user("length", "bohr", cell.T)
-        
+
         calc = tb.Calculator(
             self.method,
             self.numbers,
-            np.asarray(pos), 
+            np.asarray(pos),
             self.charge,
-            self.uhf,   #unpaired electrons
+            self.uhf,  # unpaired electrons
             np.asarray(cell) if self.periodic else None,
-            self.periodic,                         
+            self.periodic,
         )
         calc.set("verbosity", self.verbosity)
 
@@ -66,16 +69,13 @@ class TBLiteDriver(object):
         pot = results.get("energy")
         grad = results.get("gradient")
         vir = results.get("virial")
-        
 
         # converts to internal quantities
-        pot_ipi = np.asarray(
-            unit_to_internal("energy", "hartree", pot), np.float64
+        pot_ipi = np.asarray(unit_to_internal("energy", "hartree", pot), np.float64)
+        force_ipi = np.asarray(
+            unit_to_internal("force", "hartree/bohr", -grad), np.float64
         )
-        force_ipi = np.asarray(unit_to_internal("force", "hartree/bohr", - grad), np.float64)
-        vir_ipi = np.array(
-            unit_to_internal("energy", "hartree", -vir.T), np.float64
-        )
+        vir_ipi = np.array(unit_to_internal("energy", "hartree", -vir.T), np.float64)
         extras = ""
 
         return pot_ipi, force_ipi, vir_ipi, extras
