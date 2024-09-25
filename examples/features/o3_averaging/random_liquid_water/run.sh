7c17f13d7304dd8a87a15c8bbf65cdcd6fec4350
Michele Ceriotti <michele.ceriotti@gmail.com>
2024-09-26 00:31:46 +0200

    Working on the cell...

--- examples/features/o3_averaging/random_liquid_water/run.sh
+++ examples/features/o3_averaging/random_liquid_water/run.sh
@@ -1,5 +1,6 @@
 ipi=i-pi
-driver="i-pi-driver -m qtip4pf -u -a h2o-md.1"
+driver1="i-pi-driver -m noo3-h2o -u -a h2o-noo3"
+driver2="i-pi-driver -m qtip4pf -u -a h2o-base"
 sleep_time=4
 
 ${ipi} input.xml > log.i-pi & 
@@ -8,7 +9,8 @@ echo "# i-PI is running"
 echo "# Waiting for ${sleep_time} (s) before executing driver"
 sleep ${sleep_time}
 
-${driver} > /dev/null & 
+${driver1} > /dev/null & 
+${driver2} > /dev/null &
 echo "# Driver is running"
 
 wait
