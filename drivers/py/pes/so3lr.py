ee012dbc09077760e5bba628d3a7985b52990afe
Marcel Langer <me@sirmarcel.com>
2024-10-29 17:04:10 +0100

    Add SO3LR calculator
    What is says on the tin. This is pending some fixes
    on the side of So3LR and MLFF, but this is all that's
    needed from the i-pi end.

---
+++ drivers/py/pes/so3lr.py
@@ -0,0 +1,43 @@
+""" An interface for the [SO3LR](https://github.com/general-molecular-simulations/so3lr) calculator """
+
+import sys
+from .ase import ASEDriver
+
+try:
+    from so3lr import So3lrCalculator
+except:
+    So3lrCalculator = None
+
+__DRIVER_NAME__ = "so3lr"
+__DRIVER_CLASS__ = "SO3LR_driver"
+
+ERROR_MSG = """
+SO3LR driver requires a template file that describes the chemical makeup of the structure.
+
+Optionally, lr_cutoff and dispersion_energy_lr_cutoff_damping can be specified.
+
+Example: python driver.py -m so3lr -u -o template.xyz,lr_cutoff=12,dispersion_energy_lr_cutoff_damping=2
+"""
+
+
+class SO3LR_driver(ASEDriver):
+    def __init__(self, args=None, verbose=False):
+        if So3lrCalculator is None:
+            raise ImportError("Couldn't load so3lr bindings")
+
+        super().__init__(args, verbose, ERROR_MSG)
+
+    def check_arguments(self):
+        super().check_arguments()
+
+        args = self.args
+
+        kwargs = {}
+        if len(args) >= 2:
+            _ = args[0]  # template we don't need
+
+            for arg in args[1:]:
+                key, value = arg.split("=")
+                kwargs[key] = float(value)
+
+        self.ase_calculator = So3lrCalculator(**kwargs)
