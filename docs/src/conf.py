9a265e765229b9f7116788ddfce6a8c0d2250a30
BarakHirshberg <hirshb@tauex.tau.ac.il>
2024-11-11 17:46:39 +0200

    added furo to requirements.txt; changed theme to furo in conf.py

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -54,7 +54,7 @@ bibtex_bibfiles = ["references.bib"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "agogo"
+html_theme = "furo"
 html_theme_options = {
     "rightsidebar": "false",
 }
