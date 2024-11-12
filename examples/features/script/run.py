8f5de44dcdf3c34b4b09b2dd585248c031c1c3bd
Michele Ceriotti <michele.ceriotti@gmail.com>
2024-11-12 22:05:38 +0100

    Work on monkey-patching an XML file

--- examples/features/script/run.py
+++ examples/features/script/run.py
@@ -4,6 +4,26 @@ from ipi.engine.simulation import Simulation
 from ipi.utils.messages import verbosity
 import subprocess
 
+import numpy as np
+from ipi.inputs.beads import InputBeads
+import ase, ase.io
+def setup_system(xml_template, atoms):
+    pass
+
+data = ase.io.read("./water_64.xyz", 0)
+ibeads = InputBeads()
+q = data.positions.flatten()
+p = np.zeros_like(q)
+m = np.zeros(len(data))
+ibeads.q.store(q)
+ibeads.names.store(np.array(data.symbols))
+ibeads.nbeads.store(1)
+ibeads.natoms.store(len(data))
+ibeads.p.store(p)
+ibeads.m.store(m)
+print(ibeads.write("beads"))
+print(ibeads.fetch())
+
 sim = Simulation.load_from_xml(open("input.xml"))
 verbosity.level = "quiet"
 
