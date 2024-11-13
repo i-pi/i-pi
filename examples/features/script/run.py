f2c7691aaa8d2d0ff51a1468555b00ae83a3ce4e
Michele Ceriotti <michele.ceriotti@gmail.com>
2024-11-13 11:13:24 +0100

    Added infrastructure to edit an XML file

--- examples/features/script/run.py
+++ examples/features/script/run.py
@@ -5,28 +5,56 @@ from ipi.utils.messages import verbosity
 import subprocess
 
 import numpy as np
+from ipi.utils.units import Elements
 from ipi.inputs.beads import InputBeads
+from ipi.inputs.cell import InputCell
+from ipi.engine.beads import Beads
+from ipi.engine.cell import Cell
+from ipi.utils.io.inputs.io_xml import (
+    xml_node,
+    xml_parse_string,
+    xml_parse_file,
+    xml_write,
+    xml_edit,
+)
+
 import ase, ase.io
+
+
 def setup_system(xml_template, atoms):
     pass
 
-data = ase.io.read("./water_64.xyz", 0)
-ibeads = InputBeads()
-q = data.positions.flatten()
-p = np.zeros_like(q)
-m = np.zeros(len(data))
-ibeads.q.store(q)
-ibeads.names.store(np.array(data.symbols))
-ibeads.nbeads.store(1)
-ibeads.natoms.store(len(data))
-ibeads.p.store(p)
-ibeads.m.store(m)
-print(ibeads.write("beads"))
-print(ibeads.fetch())
 
-sim = Simulation.load_from_xml(open("input.xml"))
+data = ase.io.read("./water-64.xyz", 0)
+beads = Beads(nbeads=1, natoms=len(data))
+beads.q = data.positions.flatten() / 0.529177
+beads.names = np.array(data.symbols)
+beads.m = np.array([Elements.mass(n) for n in beads.names])
+
+input_beads = InputBeads()
+input_beads.store(beads)
+xml_beads = xml_parse_string(input_beads.write("beads")).fields[0][1]
+
+print("CELL ", data.cell)
+cell = Cell(h=np.array(data.cell) / 0.529177)
+input_cell = InputCell()
+input_cell.store(cell)
+xml_cell = xml_parse_string(input_cell.write("cell")).fields[0][1]
+
+xml_template = xml_parse_file("input.xml")
+
+print(xml_template)
+
+xml_edit(xml_template, ["system", "initialize"], xml_beads, mode="remove")
+xml_edit(xml_template, ["system"], xml_beads, mode="add")
+xml_edit(xml_template, ["system"], xml_cell, mode="add")
+xml_edited = xml_write(xml_template)
+print(xml_edited)
+sim = Simulation.load_from_xml(xml_edited)
 verbosity.level = "quiet"
 
+print(sim.syslist[0].beads.q[0, :3])
+print(sim.syslist[0].beads.p[0, :3])
 driver = subprocess.Popen(["i-pi-driver", "-u", "-a", "h2o-md.1", "-m", "qtip4pf"])
 
 print("# istep V K")
