8bf95ab8ef59193473145202426a847f3f400d83
Michele Ceriotti <michele.ceriotti@gmail.com>
2024-11-13 17:51:29 +0100

    linting

--- examples/features/script/run.py
+++ examples/features/script/run.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python
-from ipi.utils.scripting import simulation_xml, forcefield_xml, motion_nvt_xml, InteractiveSimulation
+from ipi.utils.scripting import (
+    simulation_xml,
+    forcefield_xml,
+    motion_nvt_xml,
+    InteractiveSimulation,
+)
 import ase, ase.io
 
 # There are utilities to quickly set up XML inputs for commonly-used simulations
@@ -10,7 +15,7 @@ input_xml = simulation_xml(
         name="harmonic", mode="direct", pes="harmonic", parameters="{k1:1e-3}"
     ),
     motion=motion_nvt_xml(timestep=0.5 * ase.units.fs),
-    temperature=400,    
+    temperature=400,
 )
 
 print("Running with XML input:\n\n", input_xml)
@@ -20,14 +25,30 @@ print("Running with XML input:\n\n", input_xml)
 sim = InteractiveSimulation(input_xml)
 
 # `properties` accesses the (well) properties of the simulation object
-print(sim.properties("time")/ase.units.fs, sim.properties("potential"), sim.properties("temperature"))
+print(
+    sim.properties("time") / ase.units.fs,
+    sim.properties("potential"),
+    sim.properties("temperature"),
+)
 # `run` advances the interactive simulation by one (or the prescribed number) of steps
 sim.run(10)
-print(sim.properties("time")/ase.units.fs, sim.properties("potential"), sim.properties("temperature"))
+print(
+    sim.properties("time") / ase.units.fs,
+    sim.properties("potential"),
+    sim.properties("temperature"),
+)
 sim.run(10)
-print(sim.properties("time")/ase.units.fs, sim.properties("potential"), sim.properties("temperature"))
+print(
+    sim.properties("time") / ase.units.fs,
+    sim.properties("potential"),
+    sim.properties("temperature"),
+)
 sim.run(10)
-print(sim.properties("time")/ase.units.fs, sim.properties("potential"), sim.properties("temperature"))
+print(
+    sim.properties("time") / ase.units.fs,
+    sim.properties("potential"),
+    sim.properties("temperature"),
+)
 
 # `get_structures` dumps the state of the system as ASE Atoms objects, possibly listing
 # all systems and beads
