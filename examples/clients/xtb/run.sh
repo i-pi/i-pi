1f7606c6e491c64c119fe8f6f70e02230a2b3467
Vahideh Alizadeh <82591913+V-Alizade@users.noreply.github.com>
2024-11-21 00:16:05 +0100

    update xtb client (#407)
    * update xtb: allow reading .xyz format
    * update tblite for version 0.4
    
    ---------
    
    Co-authored-by: Michele Ceriotti <ceriottm@users.noreply.github.com>

--- examples/clients/xtb/run.sh
+++ examples/clients/xtb/run.sh
@@ -3,5 +3,5 @@ echo "Running i-PI"
 i-pi input.xml &> log.ipi &
 sleep 1
 echo "Running driver"
-i-pi-py_driver -m xtb -o config.json -u -a xtb &> log.xtb 
+i-pi-py_driver -m xtb -o template=ch4.xyz,method=GFN2-xTB -u -a xtb &> log.xtb 
 
