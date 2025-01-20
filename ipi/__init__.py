86f0237f6d1d42b029a1238a7486a06d728391da
EliaStocco <eliastocco@gmail.com>
2025-01-20 11:03:14 +0100

    copied from Release/v3.1

--- ipi/__init__.py
+++ ipi/__init__.py
@@ -1,6 +1,14 @@
 """
-The i-PI package.
+The i-PI module.
 """
+
+# Python 3.8+:
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("ipi")
+except PackageNotFoundError:
+    __version__ = "unknown"
 
 # expose some utility functions in a more direct way
 from ipi.utils.parsing import read_output, read_trajectory
@@ -20,3 +28,10 @@ __all__ = [
 ]
 
 ipi_global_settings = {"floatformat": "%16.8e"}
+
+from ipi.engine.simulation import Simulation
+
+
+class IPI:
+    def __init__(self, xml_data):
+        self._simulation = Simulation.load_from_xml(xml_data)
