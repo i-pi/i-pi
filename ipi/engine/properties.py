9710c3305dde76716a67b9a6d0c204fa82a47fac
Venkat Kapil <venkat.kapil@gmail.com>
2025-03-04 10:01:17 +0000

    checks if enstype is string and thereby hopefully doesn't fail for multimotion

--- ipi/engine/properties.py
+++ ipi/engine/properties.py
@@ -1111,7 +1111,7 @@ class Properties:
         # accounting for constrained dof
         if self.ensemble.temp > 0:
             eff_number_fixed_dof = 0  # the effective number of fixed degrees of freedom for the selected atom(s)
-            if self.motion.enstype == "nvt-cc":
+            if isinstance(self.motion.enstype, str) and self.motion.enstype == "nvt-cc":
                 eff_number_fixed_dof = (
                     len(atom_ids) * 3
                 )  # fixing the centroid for 1 atoms effectively fixes 3 dof, therefore the eff_number_fixed_dof is 3 time the number of atoms
