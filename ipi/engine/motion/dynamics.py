22b1ce650c8b40d4342b6b06534a5a7ba4283bd8
Elia Stocco <elia.stocco@mpsd.mpg.de>
2025-06-05 16:59:42 +0200

    corrected value of dt for update_actual_time

--- ipi/engine/motion/dynamics.py
+++ ipi/engine/motion/dynamics.py
@@ -459,9 +459,8 @@ class NVEIntegrator(DummyIntegrator):
     def qcstep(self):
         """Velocity Verlet centroid position propagator."""
         # dt/inmts
-        dt = dstrip(self.qdt_on_m)
-        self.nm.qnm[0, :] += dstrip(self.nm.pnm)[0, :] * dt
-        self.update_actual_time(dt)
+        self.nm.qnm[0, :] += dstrip(self.nm.pnm)[0, :] * dstrip(self.qdt_on_m)
+        self.update_actual_time(self.qdt)
 
     # now the idea is that for BAOAB the MTS should work as follows:
     # take the BAB MTS, and insert the O in the very middle. This might imply breaking a A step in two, e.g. one could have
