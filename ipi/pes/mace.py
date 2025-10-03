c0d0ab363fa9d562840d8293b06adcc67911509f
Elia Stocco <elia.stocco@mpsd.mpg.de>
2025-10-03 15:09:33 +0200

    removed GPU

--- ipi/pes/mace.py
+++ ipi/pes/mace.py
@@ -1,9 +1,5 @@
 """An interface for the [MACE](https://github.com/ACEsuit/mace) calculator"""
 
-from ipi.pes.tools import gpu_oversubscription
-
-gpu_oversubscription()
-
 import json
 
 from .ase import ASEDriver
