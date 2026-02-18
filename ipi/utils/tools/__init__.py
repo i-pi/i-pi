6d174a0eb9bea909ed9336b335c91031826c02e7
Yair Litman <yairlitman@gmail.com>
2026-02-18 23:57:29 +0100

    black

--- ipi/utils/tools/__init__.py
+++ ipi/utils/tools/__init__.py
@@ -1,7 +1,8 @@
 from .acf_xyz import compute_acf_xyz
 from .gle import gle_frequency_kernel, isra_deconvolute, get_gle_matrices
-from .instanton_interpolation import interpolate_instanton 
+from .instanton_interpolation import interpolate_instanton
 from .instanton_postproc import instanton_compute
+
 __all__ = [
     "compute_acf_xyz",
     "gle_frequency_kernel",
@@ -9,4 +10,4 @@ __all__ = [
     "get_gle_matrices",
     "instanton_compute",
     "interpolate_instanton",
-    ]
+]
