b99d8cb892f5d097d849646da4d8b41b4c9a43aa
Yair Litman <yairlitman@gmail.com>
2026-02-19 08:50:14 +0100

    update version number to 3.1.9 (#501)
    * wrap the instantons scripts into functions for convenience
    
    * update version number
    
    * move instanton interpolation and postproc functions to utils
    
    * set some default values
    
    * bug fix
    
    * black
    
    * lint

--- ipi/utils/tools/__init__.py
+++ ipi/utils/tools/__init__.py
@@ -1,9 +1,13 @@
 from .acf_xyz import compute_acf_xyz
 from .gle import gle_frequency_kernel, isra_deconvolute, get_gle_matrices
+from .instanton_interpolation import interpolate_instanton
+from .instanton_postproc import instanton_compute
 
 __all__ = [
     "compute_acf_xyz",
     "gle_frequency_kernel",
     "isra_deconvolute",
     "get_gle_matrices",
+    "instanton_compute",
+    "interpolate_instanton",
 ]
