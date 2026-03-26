a3796e6d58ed1114e73b93038d9bf8c8e0645137
Astolfo-Official <86186998@qq.com>
2026-03-26 16:45:00 -0400

    fix: update thermostats and messages

--- ipi/inputs/thermostats.py
+++ ipi/inputs/thermostats.py
@@ -170,26 +170,6 @@ class InputThermoBase(Input):
                 "help": "Flipping type for ffl thermostat ('soft', 'hard', 'rescale', 'none')",
             },
         ),
-        # Tao E. Li modification 2021/09/28
-        "tau_m": (
-            InputValue,
-            {
-                "dtype": float,
-                "default": 0.0,
-                "help": "The friction coefficient for white noise thermostats.",
-                "dimension": "time",
-            },
-        ),
-        "tau_l": (
-            InputValue,
-            {
-                "dtype": float,
-                "default": 0.0,
-                "help": "The friction coefficient for white noise thermostats.",
-                "dimension": "time",
-            },
-        ),
-        # end of Tao E. Li modification 2021/09/28
     }
 
     dynamic = {}
@@ -253,10 +233,6 @@ class InputThermoBase(Input):
             self.mode.store("ffl")
             self.tau.store(thermo.tau)
             self.flip.store(thermo.flip)
-        elif type(thermo) is ethermostats.ThermoCavLossMultiLangevin:
-            self.mode.store("cavloss_multilangevin")
-            self.tau_m.store(thermo.tau_m._value)
-            self.tau_l.store(thermo.tau_l._value)
         elif type(thermo) is ethermostats.Thermostat:
             self.mode.store("")
         else:
@@ -322,12 +298,6 @@ class InputThermoBase(Input):
             thermo = ethermostats.ThermoFFL(
                 tau=self.tau.fetch(), flip=self.flip.fetch()
             )
-        # Start with Tao E. Li's modifications 2021/09/28
-        elif self.mode.fetch() == "cavloss_multilangevin":
-            thermo = ethermostats.ThermoCavLossMultiLangevin(
-                tau_m=self.tau_m.fetch(), tau_l=self.tau_l.fetch()
-            )
-        # End with Tao E. Li's modifications
         elif self.mode.fetch() == "":
             thermo = ethermostats.Thermostat()
         else:
@@ -367,12 +337,6 @@ class InputThermoBase(Input):
                 )
         if mode in ["gle", "nm_gle", "nm_gle_g"]:
             pass  # PERHAPS DO CHECKS THAT MATRICES SATISFY REASONABLE CONDITIONS (POSITIVE-DEFINITENESS, ETC)
-        # MR Check that pilect is not less than 0.0
-        if mode in ["cavloss_multilangevin"]:
-            if self.tau_m.fetch() < 0 or self.tau_l.fetch() < 0:
-                raise ValueError(
-                    "The thermostat friction coefficients must be set to positive values"
-                )
 
 
 class InputThermo(InputThermoBase):
